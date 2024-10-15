4713f65a95eedcb3c687b4006c63c7c27cef982c
kwak <sal091625@gmail.com>
2024-10-15 09:32:42 +0900

    feat: Promise 정리

--- frontend-practice/practice/async.js
+++ frontend-practice/practice/async.js
@@ -6,7 +6,6 @@
 // console.log('끝')
 
 // const { reject } = require("async");
-
 // function fastFunction(err, data, done){
 //     setTimeout(function(){
 //         done(undefined, data*2);
@@ -34,6 +33,35 @@
 
 
 
+// // fastFunction data*2 (1초)
+// // slowFunction data+10 (3초)
+// // initialData가 주어졌을 때, fastFunction -> slowFunction 
+// slowFunction(fastFunction(initialData))
+// function fastFunction(data, callbackFn){
+//     return setTimeout(()=>{
+//         const result = data * 2
+//         console.log("fast", result);
+//         callbackFn(result)
+//     }, 1000)
+// }
+// function slowFunction(data, callbackFn){
+//     return setTimeout(()=>{
+//         const result = data + 10;
+//         console.log("slow", result);
+//         callbackFn(result);
+//     }, 3000)
+// }
+// fastFunction(10, (data)=>{
+//     const fastFunctionResult = data;
+//     slowFunction(fastFunctionResult, data=>{
+//         const slowFunctionResult = data;
+//         console.log('fast->slow', slowFunctionResult);
+//     })
+// })
+
+
+
+// //Promise 형태
 // function fastFunction(data){
 //     return new Promise((resolve, reject)=>{
 //         setTimeout(function(){
@@ -66,30 +94,28 @@
 
 
 
-
-
-// fastFunction data*2 (1초)
-// slowFunction data+10 (3초)
-// initialData가 주어졌을 때, fastFunction -> slowFunction 
-// slowFunction(fastFunction(initialData))
-function fastFunction(data, callbackFn){
-    return setTimeout(()=>{
-        const result = data * 2
-        console.log("fast", result);
-        callbackFn(result)
-    }, 1000)
+//가장 최근 방식
+// async function은 Promise 객체이고, await는 Promise가 처리되기를 기다리는 키워드다.
+// Promise.all() => 비동기 함수를 병렬적으로 모아서 작업하고 하나라도 reject 시 전체 reject
+// Promise.race() => 하나라도 reject or resolve 시 종료
+// Promise.allSettled() => 모든 비동기 함수의 reject 혹은 resolve를 기다림
+// Promise.any() => 하나라도 성공할 때까지 기다림
+const delay = ms=>new Promise(resolve => setTimeout(resolve, ms));
+async function fastFunction(data){
+    await delay(1000);
+    const result = data*2;
+    console.log('fast!!', result);
+    return result;
 }
-function slowFunction(data, callbackFn){
-    return setTimeout(()=>{
-        const result = data + 10;
-        console.log("slow", result);
-        callbackFn(result);
-    }, 3000)
+async function slowFunction(data){
+    await delay(2000);
+    const result = data+10;
+    console.log('slow...', result);
+    return result;
 }
-fastFunction(10, (data)=>{
-    const fastFunctionResult = data;
-    slowFunction(fastFunctionResult, data=>{
-        const slowFunctionResult = data;
-        console.log('fast->slow', slowFunctionResult);
-    })
-})
+async function runTasks(){
+    let result = await fastFunction(10);
+    result = await slowFunction(result);
+    console.log('완료!!')
+}
+runTasks();
