b25e36b39eb0530713fd28e55b415814bc1194b9
kwak <sal091625@gmail.com>
2024-11-04 08:18:38 +0900

    feat:react-router proxy 설정

--- frontend-practice/react/my-react-router/my-react-router/vite.config.js
+++ frontend-practice/react/my-react-router/my-react-router/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  resolve: {
+    alias: [
+      //절대경로로 접근하기
+      { find: '~/components', replacement: '/src/components'},
+      { find: '~/lib', replacement: '/src/lib'},
+      { find: '~/pages', replacement: '/src/pages'},
+      { find: '~/routers', replacement: '/src/routers'},
+      { find: '~/routes', replacement: '/src/routes'},
+    ]
+  }
 })
